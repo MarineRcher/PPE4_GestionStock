b0e1799062b4554936b0b28ed34b6f8ff5782c1d
Marine Richer <marine.richer41@gmail.com>
2024-02-12 17:01:30 +0100

    first commit

---
+++ pages/SignIn.php
@@ -0,0 +1,9 @@
+
+<?php
+echo '<h2 id="ed">Connexion</h2>';
+echo '<form action="" method="get" id="formSignIn">';
+    echo 'Email : <input type="text" name="email" id="email">';
+    echo 'Mot de passe : <input type="text" name="password" id="password">';
+    echo '<input type="submit" value="Connexion">';
+echo '</form>'
+?>
---
+++ pages/SignUp.php
@@ -0,0 +1,11 @@
+<?php
+echo '<h2>Inscription</h2>';
+echo '<form action="" method="POST">';
+echo 'Prenom : <input type="text" name="name" id="name">';
+echo 'Nom : <input type="text" name="lastName" id="lastName">';
+echo 'Email : <input type="text" name="email" id="email">';
+echo 'Mot de passe : <input type="text" name="password" id="password">';
+echo '<input type="submit" value="Connexion">';
+echo '</form>'
+
+?>
