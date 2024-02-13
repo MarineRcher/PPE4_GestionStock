2960434df00f22a0fcab6db8246b8ac1dfa1dc33
Marine Richer <marine.richer41@gmail.com>
2024-02-13 09:50:29 +0100

    design page de connexion et inscription

--- pages/SignIn.php
+++ pages/SignIn.php
@@ -1,9 +1,24 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <title>Connexion</title>
+    <link rel="stylesheet" href="../styles/SignInAndUp.css">
 
-<?php
-echo '<h2 id="ed">Connexion</h2>';
-echo '<form action="" method="get" id="formSignIn">';
-    echo 'Email : <input type="text" name="email" id="email">';
-    echo 'Mot de passe : <input type="text" name="password" id="password">';
-    echo '<input type="submit" value="Connexion">';
-echo '</form>'
-?>
+</head>
+<body>
+<div class="containerForm">
+    <form action="" method="get" id="" class="form">
+    <h2 id="ed">Connexion</h2>
+
+        Email<input type="text" name="email" id="email" class="input">
+        Mot de passe<input type="text" name="password" id="password" class="input">
+        <input type="submit" value="Connexion" class="button">
+        <p>Vous n'avez pas de compte ? <a href="SignUp.php"><strong>Inscrivez-vous</strong><a/> </p>
+    </form>
+</div>
+</body>
+</html>
+
+
+
--- pages/SignUp.php
+++ pages/SignUp.php
@@ -1,11 +1,24 @@
-<?php
-echo '<h2>Inscription</h2>';
-echo '<form action="" method="POST">';
-echo 'Prenom : <input type="text" name="name" id="name">';
-echo 'Nom : <input type="text" name="lastName" id="lastName">';
-echo 'Email : <input type="text" name="email" id="email">';
-echo 'Mot de passe : <input type="text" name="password" id="password">';
-echo '<input type="submit" value="Connexion">';
-echo '</form>'
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <title>Connexion</title>
+    <link rel="stylesheet" href="../styles/SignInAndUp.css">
 
-?>
+</head>
+<body>
+<div class="containerForm">
+    <form action="" method="POST" id="" class="form">
+        <h2 id="ed">Inscription</h2>
+
+        Prénom<input type="text" name="name" id="name" class="input">
+        Nom de famille<input type="text" name="lastName" id="lastName" class="input">
+        Email<input type="text" name="email" id="email" class="input">
+        Mot de passe<input type="text" name="password" id="password" class="input">
+        Confirmation du mot de passe<input type="text" name="password" id="password" class="input">
+        <input type="submit" value="Connexion" class="button">
+        <p>Vous avez un compte ? <a href="SignIn.php"><strong>Connectez-vous</strong><a/> </p>
+    </form>
+</div>
+</body>
+</html>
