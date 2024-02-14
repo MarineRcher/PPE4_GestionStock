734231b19b20d05d3fd501cf230f164878f1314b
Marine Richer <marine.richer41@gmail.com>
2024-02-14 14:50:41 +0100

    footer

--- src/Vue/Footer.php
+++ src/Vue/Footer.php
@@ -1,24 +1,31 @@
-<footer>
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <title>Laboratoire GSB</title>
+    <link rel="stylesheet" href="../styles/Footer.css">
+</head>
+<body><footer>
     <div class="footer">
         <div class="row">
-            <a href="#"><i class="fa fa-facebook"></i></a>
-            <a href="#"><i class="fa fa-instagram"></i></a>
-            <a href="#"><i class="fa fa-youtube"></i></a>
-            <a href="#"><i class="fa fa-twitter"></i></a>
+            <a href="#"><img class="iconSocialMedia" src="../Assets/footer/facebook.png"/></a>
+            <a href="#"><img class="iconSocialMedia" src="../Assets/footer/instagram.png"/></a>
+            <a href="#"><img class="iconSocialMedia" src="../Assets/footer/youtube.png"/></a>
+            <a href="#"><img class="iconSocialMedia" src="../Assets/footer/twitter.png"/></a>
         </div>
 
         <div class="row">
             <ul>
-                <li><a href="#">Contact us</a></li>
-                <li><a href="#">Our Services</a></li>
-                <li><a href="#">Privacy Policy</a></li>
-                <li><a href="#">Terms & Conditions</a></li>
-                <li><a href="#">Career</a></li>
+                <li><a href="https://www.pagesjaunes.fr/">Contactez-nous</a></li>
+                <li><a href="#">Nos services</a></li>
+                <li><a href="#">Politique de confidentalité</a></li>
+                <li><a href="#">Termes & conditions</a></li>
+                <li><a href="#">Carrière</a></li>
             </ul>
         </div>
 
         <div class="row">
-            INFERNO Copyright © 2021 Inferno - All rights reserved || Designed By: Mahesh
+            LabGSB Copyright © 2024 - All rights reserved || Designed By: Marine Richer
         </div>
     </div>
 </footer>
