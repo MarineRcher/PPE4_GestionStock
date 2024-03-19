cfb7145c0520fce66aa773040a6ff784c58d6d19
Marine <marine.richer41@gmail.com>
2024-03-19 13:41:34 +0100

    change db server

--- src/Core/DataBase.php
+++ src/Core/DataBase.php
@@ -4,8 +4,8 @@ class Database {
 
     private string $host = 'db';
     private string $dbname = 'gsb';
-    private string $username = 'root';
-    private string $password = 'root';
+    private string $username = 'GSBuser';
+    private string $password = '4mC+9u}7!Q9asJ';
 
     private $dbHandler; // destine a contenir la connexion a la base de donnees (une instance de pdo)
     private $statement;
@@ -19,7 +19,7 @@ class Database {
     {
         try
         {
-            $pdo = new PDO('mysql:host=db;dbname=gsb;charset=utf8', 'root', 'root');
+            $pdo = new PDO('mysql:host=db;dbname=gsb;charset=utf8mb4', 'GSBuser', '4mC+9u}7!Q9asJ',array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8") );
             $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
             return $pdo;
@@ -32,11 +32,12 @@ class Database {
     public function __construct()
     {
 
-        $dsn = 'mysql:host=' . $this->host . ';dbname=' . $this->dbname . ';charset=utf8';
+        $dsn = 'mysql:host=' . $this->host . ';dbname=' . $this->dbname . ';charset=utf8mb4';
 
         $options = array(
             PDO::ATTR_PERSISTENT => true,
-            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"
         );
 
         // Create a new PDO instance
